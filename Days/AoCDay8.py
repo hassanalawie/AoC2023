37af69553b702f81469d52a8d62afe4c9c0f86dc
hassanalawie <hassan.alawie10@gmail.com>
2023-12-10 18:40:34 -0500

    Day 8

--- Days/AoCDay8.py
+++ Days/AoCDay8.py
@@ -5,17 +5,20 @@ class AoC():
             "R":1,
             "L":0
         }
-        self.current_instruction = 0
-        self.current_location = ""
+        self.current_location = "AAA"
         self.maps = {}
         self.instructions = ""
         self.step_count = 0
 
-    def update_current_instruction(self):
-        if self.current_instruction == len(self.instructions) - 1:
-            self.current_instruction = 0
-        else:
-            self.current_instruction += 1
+    def get_start_nodes(self):
+        sn = []
+        for key in self.maps.keys():
+            if key[-1] == "A":
+                sn.append((key, self.maps[key]))
+        return sn
+
+    def get_current_instruction(self):
+        return(self.step_count - 1) % len(self.instructions)
 
     def parse_file(self):
         self.instructions = self.network[0][:-1]
@@ -26,27 +29,50 @@ class AoC():
             value = value[:-1]
             value_tuple = (value[1:4], value[6:9])
             self.maps[key] = value_tuple
-            if first:
-                first = False
-                self.current_location = key
-        
+    def __gcd(self, a, b):
+        if (a == 0):
+            return b
+        return self.__gcd(b % a, a)
+    def LcmOfArray(self, arr, idx):
+        # lcm(a,b) = (a*b/gcd(a,b))
+        if (idx == len(arr)-1):
+            return arr[idx]
+        a = arr[idx]
+        b = self.LcmOfArray(arr, idx+1)
+        return int(a*b/self.__gcd(a,b))
+
     def dayEightPartOne(self):
         self.parse_file()
 
         while self.current_location != "ZZZ":
             self.step_count += 1
             options_from_current_location = self.maps[self.current_location]
-            direction = self.instructions[self.current_instruction]
+            direction = self.instructions[self.get_current_instruction()]
             right_or_left = self.instruction_map[direction]
             self.current_location = options_from_current_location[right_or_left]
 
-            self.update_current_instruction()
-
         print(f"Final Answer: {self.step_count}")
+
     def dayEightPartTwo(self):
-        return 1
+        self.parse_file()
+        start_nodes = self.get_start_nodes()
+        print("hi")
+        steps = []
+        for node in start_nodes:
+            self.current_location = node[0]
+            while self.current_location[-1] != "Z":
+                self.step_count += 1
+                options_from_current_location = self.maps[self.current_location]
+                direction = self.instructions[self.get_current_instruction()]
+                right_or_left = self.instruction_map[direction]
+                self.current_location = options_from_current_location[right_or_left]
+            steps.append(self.step_count)
+            self.__init__()
+            self.parse_file()
+        print(f"Final Answer: {self.LcmOfArray(steps,0)}")
+
     
 if __name__ == '__main__':
     advent = AoC()
-    advent.dayEightPartOne()
+    # advent.dayEightPartOne()
     advent.dayEightPartTwo()
